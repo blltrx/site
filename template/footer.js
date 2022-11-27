01683b57fd1b8eae7a6283c69afd0363159d43b3
blltrx <roze.swn@gmail.com>
2022-11-27 11:30:25 +0000

    Website framework update 1

---
+++ template/footer.js
@@ -0,0 +1,48 @@
+document.write('\
+    <footer class="footer">\
+        <div class="inner">\
+            <div class="column is-logo">\
+                <a href="index.html" class="main-logo">\
+                    <div class="logo">\
+                        <img src="assets/logo.webp" alt="logo">\
+                    </div>\
+                    <div class="logo-info">\
+                        <div class="text">bellatrix.dev</div>\
+                        <span class="copyright">Sonya Nye © MMXXII</span>\
+                    </div>\
+                </a>\
+            </div>\
+            <div class="column is-nav">\
+                <div class="column-title">Navigation</div>\
+                <ul>\
+                    <li><aa class="footlink" href="index.html">Home</a></li>\
+                    <li><aa class="footlink" href="w.html">Writing</a></li>\
+                    <li><aa class="footlink" href="v.html">Video</a></li>\
+                    <li><aa class="footlink" href="c.html">Contact</a></li>\
+                </ul>\
+            </div>\
+            <div class="column is-nav">\
+                <div class="column-title">Social Media</div>\
+                <ul>\
+                    <li><aa class="footlink" href="www.twitter.com/commiecatgirl7"><i class="fa fa-envelope-open"></i>Twitter</a></li>\
+                    <li><aa class="footlink" href="www.instagram.com/commiecatgirl7"><i class="fa fa-twitter"></i>Instagram</a></li>\
+                    <li><aa class="footlink" href="#"><i class="fa fa-twitter"></i>Sonya Nye#7997</a></li>\
+                    <li><aa class="footlink" href="www.github.com/blltrx"><i class="fa fa-linkedin"></i>Github</a></li>\
+                    <li><aa class="footlink" href="https://www.youtube.com/channel/UCC0YaHu--x6iRxl1p2HwF5A"><i class = "fa fa-twitter"></i>Youtube</i></aa></li>\
+                </ul>\
+                <div class="column-title">More</div>\
+                <ul>\
+                    <li><aa class="footlink" href="https://redoesports.webflow.io/">RE:DO Esports</a></li>\
+                </ul>\
+            </div>\
+            <div class="column is-nav">\
+                <div class="column-title">Other</div>\
+                <ul>\
+                    <li><aa class="footlink" href="other/styleguide.html">styleguide</a></li>\
+                    <li><aa class="footlink" href="other/metapost.html">metapost</a></li>\
+                    <li><aa class="footlink" href="other/twitter.html">twitter archive</a></li>\
+                </ul>\
+            </div>\
+        </div>\
+    </footer>\
+')
---
+++ template/header.js
@@ -0,0 +1,23 @@
+
+document.write('\
+    <!-- Large Screen Navigation Bar -->\
+    <nav>\
+        <div class="logo">\
+        <img class="headlogo" src = "assets/logo.webp" alt="logo">\
+        </div>\
+        <div class="links">\
+        <a class = "headlink" src = w.html>Writing</a>\
+        <a class = "headlink" src = v.html>Video</a>\
+        <a class = "headlink" src = c.html>Contact</a>\
+        <a class = "headlink" src = index.html>Home</a>\
+        <strong id="mobile-menu-btn">Menu</strong>\
+        </div>\
+    </nav>\
+    <!-- Small Screen Navigation Bar -->\
+    <div class="mobile-menu">\
+        <a class = "headlinkm" src = index.html>Home</a>\
+        <a class = "headlinkm" src = w.html>Writing</a>\
+        <a class = "headlinkm" src = v.html>Video</a>\
+        <a class = "headlinkm" src = c.html>Contact</a>\
+    </div>\
+  ');
