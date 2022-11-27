6ac8bbc507aee6b56a0fec03c19cc2df5d4be9e6
blltrx <roze.swn@gmail.com>
2022-11-27 11:54:24 +0000

    minor ux change

--- template/header.js
+++ template/header.js
@@ -6,10 +6,10 @@ document.write('\
         <img class="headlogo" src = "assets/logo.webp" alt="logo">\
         </div>\
         <div class="links">\
+        <a class = "headlink" src = index.html>Home</a>\
         <a class = "headlink" src = w.html>Writing</a>\
         <a class = "headlink" src = v.html>Video</a>\
         <a class = "headlink" src = c.html>Contact</a>\
-        <a class = "headlink" src = index.html>Home</a>\
         <strong id="mobile-menu-btn">Menu</strong>\
         </div>\
     </nav>\
