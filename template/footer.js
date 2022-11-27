7aa996604625d46870bb3b0a2edc8d253a6c68da
blltrx <roze.swn@gmail.com>
2022-11-27 11:36:40 +0000

    fixing footer links

--- template/footer.js
+++ template/footer.js
@@ -15,32 +15,32 @@ document.write('\
             <div class="column is-nav">\
                 <div class="column-title">Navigation</div>\
                 <ul>\
-                    <li><aa class="footlink" href="index.html">Home</a></li>\
-                    <li><aa class="footlink" href="w.html">Writing</a></li>\
-                    <li><aa class="footlink" href="v.html">Video</a></li>\
-                    <li><aa class="footlink" href="c.html">Contact</a></li>\
+                    <li><a class="footlink" href="index.html">Home</a></li>\
+                    <li><a class="footlink" href="w.html">Writing</a></li>\
+                    <li><a class="footlink" href="v.html">Video</a></li>\
+                    <li><a class="footlink" href="c.html">Contact</a></li>\
                 </ul>\
             </div>\
             <div class="column is-nav">\
                 <div class="column-title">Social Media</div>\
                 <ul>\
-                    <li><aa class="footlink" href="www.twitter.com/commiecatgirl7"><i class="fa fa-envelope-open"></i>Twitter</a></li>\
-                    <li><aa class="footlink" href="www.instagram.com/commiecatgirl7"><i class="fa fa-twitter"></i>Instagram</a></li>\
-                    <li><aa class="footlink" href="#"><i class="fa fa-twitter"></i>Sonya Nye#7997</a></li>\
-                    <li><aa class="footlink" href="www.github.com/blltrx"><i class="fa fa-linkedin"></i>Github</a></li>\
-                    <li><aa class="footlink" href="https://www.youtube.com/channel/UCC0YaHu--x6iRxl1p2HwF5A"><i class = "fa fa-twitter"></i>Youtube</i></aa></li>\
+                    <li><a class="footlink" href="https://www.twitter.com/commiecatgirl7" target="_blank" rel="noopener noreferrer"><i class="fa fa-envelope-open"></i>Twitter</a></li>\
+                    <li><a class="footlink" href="https://www.instagram.com/commiecatgirl7" target="_blank" rel="noopener noreferrer"><i class="fa fa-twitter"></i>Instagram</a></li>\
+                    <li><a class="footlink" href="#"><i class="fa fa-twitter"></i>Sonya Nye#7997</a></li>\
+                    <li><a class="footlink" href="https://www.github.com/blltrx" target="_blank" rel="noopener noreferrer"><i class="fa fa-linkedin"></i>Github</a></li>\
+                    <li><a class="footlink" href="https://www.youtube.com/channel/UCC0YaHu--x6iRxl1p2HwF5A" target="_blank" rel="noopener noreferrer"><i class = "fa fa-twitter"></i>Youtube</i></a></li>\
                 </ul>\
                 <div class="column-title">More</div>\
                 <ul>\
-                    <li><aa class="footlink" href="https://redoesports.webflow.io/">RE:DO Esports</a></li>\
+                    <li><a class="footlink" href="https://redoesports.webflow.io/" target="_blank" rel="noopener noreferrer">RE:DO Esports</a></li>\
                 </ul>\
             </div>\
             <div class="column is-nav">\
                 <div class="column-title">Other</div>\
                 <ul>\
-                    <li><aa class="footlink" href="other/styleguide.html">styleguide</a></li>\
-                    <li><aa class="footlink" href="other/metapost.html">metapost</a></li>\
-                    <li><aa class="footlink" href="other/twitter.html">twitter archive</a></li>\
+                    <li><a class="footlink" href="other/styleguide.html">styleguide</a></li>\
+                    <li><a class="footlink" href="other/metapost.html">metapost</a></li>\
+                    <li><a class="footlink" href="other/twitter.html">twitter archive</a></li>\
                 </ul>\
             </div>\
         </div>\
